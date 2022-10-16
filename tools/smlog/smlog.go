f2449ce68c11bb38590fe2725b1d5fa740373ada
massimostanzione <massimo.stanzione@hotmail.it>
2022-10-16 22:53:24 +0200

    close #11

--- tools/smlog/smlog.go
+++ tools/smlog/smlog.go
@@ -12,69 +12,6 @@ func SetState(state string) {
 	curState = state
 }
 
-//TODO come event in altro file?
-type LogEvent uint8
-
-const (
-	LOG_UNDEFINED LogEvent = iota
-	LOG_MSG_SENT
-	LOG_MSG_RECV
-	LOG_HB
-	LOG_NETWORK
-	LOG_SERVREG
-	LOG_STATEMACHINE
-	LOG_ELECTION
-)
-
-// String implements Stringer.
-func (event LogEvent) String() string {
-	switch event {
-	case LOG_UNDEFINED:
-		return "LOG_UNDEFINED"
-	case LOG_MSG_SENT:
-		return "LOG_MSG_SENT"
-	case LOG_MSG_RECV:
-		return "LOG_MSG_RECV"
-	case LOG_HB:
-		return "LOG_HB"
-	case LOG_NETWORK:
-		return "LOG_NETWORK"
-	case LOG_SERVREG:
-		return "LOG_SERVREG"
-	case LOG_STATEMACHINE:
-		return "LOG_STATEMACHINE"
-	case LOG_ELECTION:
-		return "LOG_ELECTION"
-	default:
-		return "<unknown>"
-	}
-}
-
-// Short returns a five character string to use in
-// aligned logging output.
-func (event LogEvent) Short() string {
-	switch event {
-	case LOG_UNDEFINED:
-		return "N/D  "
-	case LOG_MSG_SENT:
-		return "MSENT"
-	case LOG_MSG_RECV:
-		return "MRECV"
-	case LOG_HB:
-		return "HB   "
-	case LOG_NETWORK:
-		return "NETWK"
-	case LOG_SERVREG:
-		return "SVREG"
-	case LOG_STATEMACHINE:
-		return "STMAC"
-	case LOG_ELECTION:
-		return "ELECT"
-	default:
-		return "<unknown>"
-	}
-}
-
 var loggoLogger = loggo.GetLogger("")
 
 func Trace(typee LogEvent, message string, args ...interface{}) {
