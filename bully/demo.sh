cb126b83af679ed596942fa27c2306e413946df3
massimostanzione <massimo.stanzione@hotmail.it>
2022-10-23 18:56:30 +0200

    bully-rebase

--- bully/demo.sh
+++ bully/demo.sh
@@ -32,12 +32,12 @@ done
 base_addr_space=40042
 workersno=10
 # TODO test workersno >=4, come da requisiti
-    $detected_term -T "ServReg" -e "bash -c './cmd/centrale/centrale;bash'"   
+    $detected_term -T "ServReg" -e "bash -c './bin/serviceRegistry;bash'"   
 for i in $(seq 1 $workersno)
 do
 # inserisco ritardo random per evitare che tutti si connettano insieme (TODO risolvere)
 #sleep  $[ ( $RANDOM % 1 )  + 1 ]s
-    $detected_term -T "NODO n. $i"    -e "bash -c './cmd/client/client -p $(($base_addr_space + $i));bash'"    
+    $detected_term -T "NODO n. $i"    -e "bash -c './bin/node -p $(($base_addr_space + $i));bash'"    
 done
 # run client
 #$detected_term -T "ServReg" -e "bash -c 'echo -E In order to run the client please run:;echo -E ./client;echo -E or see:;echo -E ./#client -help;echo -E for all the available flags;cd client/bin;bash'"
