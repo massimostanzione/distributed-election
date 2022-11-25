5ffae89e3e201799816a7594e7b1becf6c95d9eb
massimostanzione <massimo.stanzione@hotmail.it>
2022-11-25 16:54:39 +0100

    ref. #94

--- examples/bully.sh
+++ examples/bully.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # a quick, ready-to-use demo
 
-NODES_NO=4
+NODES_NO=12
 BASE_ADDRESS=40042
 
 echo "*** distributed-election demo ***"
@@ -38,5 +38,5 @@ $detected_term -T "ServReg" -e "bash -c './../bin/serviceregistry;bash'"
 #run nodes
 for i in $(seq 1 $NODES_NO)
 do
-    $detected_term -T "Node n. $i" -e "bash -c './../bin/node -c ../configs/config.ini -v -a b -ncl ABSENT -p $(($BASE_ADDRESS + $i));bash'"    
+    $detected_term -T "Node n. $i" -e "bash -c './../bin/node -c ../configs/config.ini -a b -ncl ABSENT -p $(($BASE_ADDRESS + $i));bash'"    
 done
